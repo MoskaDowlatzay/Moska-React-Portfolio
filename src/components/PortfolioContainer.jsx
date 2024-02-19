31e86fcc730944146ea2706df19eead499019b8f
MoskaDowlatzay <moskadowlatzay@hotmail.com>
2024-02-19 23:01:04 +0000

    added footer

--- src/components/PortfolioContainer.jsx
+++ src/components/PortfolioContainer.jsx
@@ -25,7 +25,16 @@ function PortfolioContainer() {
     <div>
       <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
       {renderPage()}    
-      //footer
+      {/* footer     */}
+      <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet"/>
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet"/>
+<footer class="footer">
+  <div class="container text-center">
+    <a href="https://github.com/MoskaDowlatzay"><i class="fa fa-github"></i></a>
+    <a href="https://twitter.com/?lang=en"><i class="bi--twitter-x"></i></a>
+    <a href="https://www.linkedin.com/in/moska-dowlatzay/"><i class="fa fa-linkedin"></i></a>
+  </div>
+</footer>
     </div>
 
   );
